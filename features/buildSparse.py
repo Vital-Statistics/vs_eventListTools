36a539d5c724b4502c7edd793823720b31cdb393
Joseph Lucas <joe.statistics@gmail.com>
2021-02-01 15:22:53 -0500

    bug fix in buildSparse due to updates to Python.

--- features/buildSparse.py
+++ features/buildSparse.py
@@ -5,23 +5,39 @@ Created on Wed Jun  7 10:29:12 2017
 @author: jel2
 """
 
-def buildSparse(df,a,b,colFilter=None,rowFilter=None):
-    ct=df[[a,b]].groupby([a,b]).size()
-    i=np.array(ct.index.labels[0])
-    j=np.array(ct.index.labels[1])
-    rLbl=list(ct.index.levels[0])
-    cLbl=list(ct.index.levels[1])
-    M=scipy.sparse.coo_matrix((np.array(ct),(i,j)),shape=(len(rLbl),len(cLbl)))
+def buildSparse(df,a,b,colFilter=None,rowFilter=None,makeBinary=False,rowLabels=None,colLabels=None):
+    import scipy
+    import pandas as pd
+    import numpy as np
+    ct=df[[a,b]].groupby([a,b]).size().rename('value').reset_index()
+    if rowLabels is not None:
+        ct=ct.loc[ct[a].isin(rowLabels)]
+        rowLabels=list(rowLabels)
+    else:
+        rowLabels=list(pd.Series(ct[a].unique()).sort_values())
+    if colLabels is not None:
+        ct=ct.loc[ct[b].isin(colLabels)]
+        colLabels=list(colLabels)
+    else:
+        colLabels=list(pd.Series(ct[b].unique()).sort_values())
+    x=pd.Categorical(ct[a],categories=rowLabels)
+    y=pd.Categorical(ct[b],categories=colLabels)
+    i=x.codes
+    j=y.codes
+    rLbl=pd.Series(x.categories)
+    cLbl=pd.Series(y.categories)
+    if makeBinary: ct.value=1
+    M=scipy.sparse.coo_matrix((np.array(ct.value),(i,j)),shape=(len(rLbl),len(cLbl)))
     M=M.tocsr()
     
     if colFilter:
         kp=np.asarray(M.sum(axis=0)).reshape(-1)>=colFilter
         M=M[:,kp]
-        cLbl=list(np.asarray(cLbl)[kp])
+        cLbl=cLbl.loc[kp]
 
     if rowFilter:
         kp=np.asarray(M.sum(axis=1)).reshape(-1)>=rowFilter
         M=M[kp,:]
-        cLbl=list(np.asarray(rLbl)[kp])
+        rLbl=rLbl.loc[kp]
     
     return([rLbl,cLbl,M])
---
+++ visualizations/vs_clr.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Feb  1 10:47:06 2021
+
+@author: JoeLucas
+"""
+
+def vs_clr(cc='blue'):
+    r=(15/256,98/256,160/256)
+    if(cc=='dark blue'):
+        r=(48/256,48/256,95/256)
+    elif((cc=='grey') | (cc=='gray')):
+        r=(182/256,183/256,182/256)
+    elif((cc=='dark grey') | (cc=='dark gray')):
+        r=(84/256,85/256,84/256)
+    return(r)
