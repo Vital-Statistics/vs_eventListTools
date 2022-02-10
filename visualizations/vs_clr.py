f5a55929b818c725a1d46f84a723f22aea91b90e
zm00094 <71019813+zm00094@users.noreply.github.com>
2022-02-10 12:59:51 -0500

    minor

--- visualizations/vs_clr.py
+++ visualizations/vs_clr.py
@@ -5,7 +5,7 @@ Created on Mon Feb  1 10:47:06 2021
 @author: JoeLucas
 """
 
-def vs_clr(cc='blue'):
+def vs_clr(cc='blue',alpha=1):
     r=(15/256,98/256,160/256)
     if(cc=='dark blue'):
         r=(48/256,48/256,95/256)
@@ -13,4 +13,6 @@ def vs_clr(cc='blue'):
         r=(182/256,183/256,182/256)
     elif((cc=='dark grey') | (cc=='dark gray')):
         r=(84/256,85/256,84/256)
+    if alpha<1:
+        r=tuple(list(r)+[alpha])
     return(r)
